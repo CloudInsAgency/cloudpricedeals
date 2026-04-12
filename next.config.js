2cde22b1178709ace0c07bcc3e65e1bf1edf5106
CloudInsAgency <cfoskey@thecloudins.com>
2026-04-12 12:36:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com'],
+    domains: [
+      'images.unsplash.com',
+      'via.placeholder.com',
+      'm.media-amazon.com',
+      'images-na.ssl-images-amazon.com',
+      'wsrv.nl',
+    ],
   },
 }
 module.exports = nextConfig
